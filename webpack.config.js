01f5049b272c8d51e2f8441a0fc68dee366534de
Nazar Yaremii <n.yaremiy791@gmail.com>
2023-02-17 13:57:03 +0200

    Added settings for working with pictures

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -55,11 +56,40 @@ module.exports = {
         ],
       },
       {
-          test: /\.ttf?$/i,
-          type: 'asset/resource',
-          generator: {
-            filename : 'fonts/[name][ext]'
-          }
+        test: /\.ttf?$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.(jpe?g|png|webp|gif|svg)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+        type: 'asset/resource',
       },
       {
         test: /\.m?js$/i,
